6684c97bf5aafccf9f8a782faa7c59cef9960b3a
Ubuntu <ubuntu@ip-172-26-5-91.ec2.internal>
2020-03-15 17:53:12 +0000

    Continued investigations into pwn3

--- PWN/pwn3/hack.py
+++ PWN/pwn3/hack.py
@@ -2,10 +2,8 @@ from pwn import *
 
 p = process("./pwn_me")
 
-def first_overflow():
-    while True:
-        p.sendlineafter(b'PROMPT>', b'A'*4000)
-        print(p.recv())
+def hop_in_debugger():
+    p.interactive()
 
 if __name__=='__main__':
-    first_overflow()
+    hop_in_debugger()
